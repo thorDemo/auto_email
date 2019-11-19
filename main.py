a95f2d9ebf387d2524f1785bbb192994755dc7a5
thorDemo <40915413+thorDemo@users.noreply.github.com>
2019-11-19 14:23:38 +0800

    templates

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import uuid
 import smtplib
 from email.mime.text import MIMEText
 from email.header import Header
+from mylib.coder import encode_header
 
 sender = 'serivces@jnyhldw.com'
 subject = '宝马会礼金大放送'
@@ -16,15 +17,14 @@ content = open('templates/type_1.html', encoding='utf-8')
 message = MIMEText(content.read(), _subtype='html', _charset='utf-8')
 message['Accept-Language'] = "zh-CN"
 message['Accept-Charset'] = "ISO-8859-1,UTF-8"
-message['From'] = '宝马会娱乐城'
-message['To'] = '914081010@qq.com'    # 接收者
+message['From'] = encode_header('宝马会娱乐城', sender)
+message['To'] = encode_header('超级VIP客户', '914081010@qq.com')
 message['Subject'] = Header(subject, 'utf-8')
 message['Received'] = 'from msc-channel180022225.sh(100.68.112.227) by smtp.aliyun-inc.com(127.0.0.1);'
 message['Message-ID'] = uuid.uuid4().__str__()
 message['MIME-Version'] = '1.0'
 message['Return-Path'] = 'smtp.jnyhldw.com'
-message.add_header('From', '<serivces@jnyhldw.com>')
-message.add_header('To', '<914081010@qq.com>')
+
 
 try:
     service = smtplib.SMTP('localhost')
@@ -32,5 +32,5 @@ try:
     service.quit()
     print("邮件发送成功")
 except smtplib.SMTPException as e:
-    print(e)
+
     print("Error: 无法发送邮件")
