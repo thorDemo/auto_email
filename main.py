681e680f54bc4988bb3bbfcbafbef6b56cebd416
thorDemo <40915413+thorDemo@users.noreply.github.com>
2019-11-19 13:18:29 +0800

    templates

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import uuid
 import smtplib
 from email.mime.text import MIMEText
 from email.header import Header
-from email.headerregistry import Address
 
 sender = 'serivces@jnyhldw.com'
 subject = '宝马会礼金大放送'
@@ -17,8 +16,8 @@ content = open('templates/type_1.html', encoding='utf-8')
 message = MIMEText(content.read(), _subtype='html', _charset='utf-8')
 message['Accept-Language'] = "zh-CN"
 message['Accept-Charset'] = "ISO-8859-1,UTF-8"
-message['From'] = Header(('宝马会娱乐城'.encode('utf-8'), 'utf-8'), (b'<serivces@jnyhldw.com>', None))  # 发送者
-message['To'] = Header(('超级VIP'.encode('utf-8'), 'utf-8'), ('<914081010@qq.com>', 'UTF-8'))    # 接收者
+message['From'] = Header(bytes('宝马会娱乐城 <serivces@jnyhldw.com>', encoding='utf-8'), 'utf-8')  # 发送者
+message['To'] = Header(bytes('超级VIP客户 <914081010@qq.com>', encoding='utf-8'), 'utf-8')    # 接收者
 message['Subject'] = Header(subject, 'utf-8')
 message['Received'] = 'from msc-channel180022225.sh(100.68.112.227) by smtp.aliyun-inc.com(127.0.0.1);'
 message['Message-ID'] = uuid.uuid4().__str__()
