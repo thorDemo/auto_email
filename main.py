5ac1b53a825770330d7751a0a313650684996c11
thorDemo <40915413+thorDemo@users.noreply.github.com>
2019-11-19 14:13:19 +0800

    templates

--- main.py
+++ main.py
@@ -16,14 +16,16 @@ content = open('templates/type_1.html', encoding='utf-8')
 message = MIMEText(content.read(), _subtype='html', _charset='utf-8')
 message['Accept-Language'] = "zh-CN"
 message['Accept-Charset'] = "ISO-8859-1,UTF-8"
-message['From'] = '=?UTF-8?B?5a6d6ams5Lya5aix5LmQ?= <serivces@jnyhldw.com>'
+message['From'] = '宝马会娱乐城'
 message['To'] = '914081010@qq.com'    # 接收者
 message['Subject'] = Header(subject, 'utf-8')
 message['Received'] = 'from msc-channel180022225.sh(100.68.112.227) by smtp.aliyun-inc.com(127.0.0.1);'
 message['Message-ID'] = uuid.uuid4().__str__()
 message['MIME-Version'] = '1.0'
 message['Return-Path'] = 'smtp.jnyhldw.com'
-print(message.as_string())
+message.add_header('From', '<serivces@jnyhldw.com>')
+message.add_header('To', '<914081010@qq.com>')
+
 try:
     service = smtplib.SMTP('localhost')
     service.sendmail(sender, receivers, message.as_string())
