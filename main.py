3b752f90704689fa1e46525d900176c8c7701703
thorDemo <40915413+thorDemo@users.noreply.github.com>
2019-11-19 19:07:55 +0800

    thread pool 5

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from email.header import Header
 from mylib.coder import encode_header
 from mylib.code_logging import Logger
 from mylib.random_chars import random_chars
+from threadpool import ThreadPool, makeRequests
 
 
 class SMTPError(SMTP):
@@ -53,57 +54,71 @@ class SMTPError(SMTP):
         return senderrs
 
 
-domain = 'jnyhldw.com'
-logging = Logger('send_email.log').get_log()
-sender = f'service_{random_chars(4)}@{domain}'
-file = open('target/111901.txt', 'r', encoding='utf-8')
-temp = 0
-for line in file:
+def send_message(_temp, _receivers, _sender):
     try:
-        receivers = line.strip()
         content = open('templates/type_1.html', encoding='utf-8')
         message = MIMEText(content.read(), _subtype='html', _charset='utf-8')
         message['Accept-Language'] = "zh-CN"
         message['Accept-Charset'] = "ISO-8859-1,UTF-8"
-        message['From'] = encode_header('宝马会娱乐城', sender)
-        message['To'] = encode_header('超级VIP客户', receivers)
+        message['From'] = encode_header('宝马会娱乐城', _sender)
+        message['To'] = encode_header('超级VIP客户', _receivers)
         message['Subject'] = Header('宝马会礼金大放送', 'utf-8')
         message['Received'] = f'from msc-channel180022225.sh(100.68.112.227) by smtp.{domain}(127.0.0.1);'
         message['Message-ID'] = uuid.uuid4().__str__()
         message['MIME-Version'] = '1.0'
         message['Return-Path'] = f'smtp.{domain}'
         service = SMTPError('localhost')
-        data = service.sendmail(sender, receivers, message.as_string())
+        data = service.sendmail(_sender, _receivers, message.as_string())
         print(data)
-        temp += 1
-        logging.info(f'{receivers} 第 {temp} 封邮件发送成功！')
-        if temp % 20 == 0:
+        logging.info(f'{_receivers} 第 {_temp} 封邮件发送成功！')
+        if _temp % 20 == 0:
             return_back = MIMEText(content.read(), _subtype='html', _charset='utf-8')
             return_back['Accept-Language'] = "zh-CN"
             return_back['Accept-Charset'] = "ISO-8859-1,UTF-8"
-            return_back['From'] = encode_header('回测邮件', sender)
-            return_back['To'] = encode_header('超级VIP客户', receivers)
-            return_back['Subject'] = Header(f'{temp}:回测邮件_赵四', 'utf-8')
+            return_back['From'] = encode_header('回测邮件', _sender)
+            return_back['To'] = encode_header('超级VIP客户', _receivers)
+            return_back['Subject'] = Header(f'{_temp}:回测邮件_赵四', 'utf-8')
             return_back['Received'] = f'from msc-channel180022225.sh(100.68.112.227) by smtp.{domain}(127.0.0.1);'
             return_back['Message-ID'] = uuid.uuid4().__str__()
             return_back['MIME-Version'] = '1.0'
             return_back['Return-Path'] = 'smtp.jnyhldw.com'
-            data = service.sendmail(sender, '914081010@qq.com', return_back.as_string())
+            data = service.sendmail(_sender, '914081010@qq.com', return_back.as_string())
+            logging.info(f'{_temp}_{_receivers} 第 {_temp} 封邮件发送成功！')
+            print(data)
     except ConnectionRefusedError:
-        logging.warning(f'{receivers} 无法连接本地服务器。')
+        logging.warning(f'{_temp}_{_receivers} 无法连接本地服务器。')
     except smtplib.SMTPServerDisconnected:
-        logging.warning(f'{receivers} 服务器意外断开连接。')
+        logging.warning(f'{_temp}_{_receivers} 服务器意外断开连接。')
     except smtplib.SMTPSenderRefused:
-        logging.warning(f'{receivers} 发件人地址被拒绝。')
+        logging.warning(f'{_temp}_{_receivers} 发件人地址被拒绝。')
     except smtplib.SMTPRecipientsRefused:
-        logging.warning(f'{receivers} 所有收件人地址均被拒绝。')
+        logging.warning(f'{_temp}_{_receivers} 所有收件人地址均被拒绝。')
     except smtplib.SMTPDataError:
-        logging.warning(f'{receivers} SMTP服务器拒绝接受邮件数据。')
+        logging.warning(f'{_temp}_{_receivers} SMTP服务器拒绝接受邮件数据。')
     except smtplib.SMTPConnectError:
-        logging.warning(f'{receivers} 与服务器建立连接期间发生错误。')
+        logging.warning(f'{_temp}_{_receivers} 与服务器建立连接期间发生错误。')
     except smtplib.SMTPHeloError:
-        logging.warning(f'{receivers} 服务器拒绝了我们的HELO消息。')
+        logging.warning(f'{_temp}_{_receivers} 服务器拒绝了我们的HELO消息。')
     except smtplib.SMTPNotSupportedError:
-        logging.warning(f'{receivers} 服务器不支持尝试的命令或选项。')
+        logging.warning(f'{_temp}_{_receivers} 服务器不支持尝试的命令或选项。')
     except smtplib.SMTPAuthenticationError:
-        logging.warning(f'{receivers} 服务器不支持尝试的命令或选项。')
+        logging.warning(f'{_temp}_{_receivers} 服务器不支持尝试的命令或选项。')
+
+
+domain = 'jnyhldw.com'
+logging = Logger('send_email.log').get_log()
+file = open('target/111901.txt', 'r', encoding='utf-8')
+emails = []
+for line in file:
+    emails.append(line.strip())
+
+pool = ThreadPool(5)
+arg = []
+temp = 0
+for email in emails:
+    arg.append(([temp, email, f'service_{random_chars(4)}@{domain}'], None))
+    temp += 1
+
+request = makeRequests(send_message, arg)
+[pool.putRequest(req) for req in request]
+pool.wait()
