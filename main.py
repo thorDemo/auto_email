47144c16a3d70692a93e255d21cde6ec011c29e0
thorDemo <40915413+thorDemo@users.noreply.github.com>
2019-11-19 11:39:23 +0800

    templates

--- main.py
+++ main.py
@@ -7,15 +7,15 @@ from email.header import Header
 sender = 'serivces@jnyhldw.com'
 subject = '宝马会礼金大放送'
 receivers = [
-    '914060505@qq.com',
-    '914030606@qq.com',
-    '914820606@qq.com',
+    # '914060505@qq.com',
+    # '914030606@qq.com',
+    # '914820606@qq.com',
     '914081010@qq.com',
 ]
 content = open('templates/type_1.html', encoding='utf-8')
 message = MIMEText(content.read(), _subtype='html', _charset='utf-8')
 message['From'] = Header("宝马会 <serivces@jnyhldw.com>", 'utf-8')   # 发送者
-message['To'] = Header("玩家", 'utf-8')        # 接收者
+message['To'] = Header("超级VIP <914081010@qq.com>", 'utf-8')        # 接收者
 message['Subject'] = Header(subject, 'utf-8')
 
 try:
