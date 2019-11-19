dc242044b677c8ef9787a952023cadeb35d19dd2
thorDemo <40915413+thorDemo@users.noreply.github.com>
2019-11-19 12:39:42 +0800

    templates

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ content = open('templates/type_1.html', encoding='utf-8')
 message = MIMEText(content.read(), _subtype='html', _charset='utf-8')
 message['Accept-Language'] = "zh-CN"
 message['Accept-Charset'] = "ISO-8859-1,UTF-8"
-message['From'] = Header(u'baomahui <serivces@jnyhldw.com>', 'UTF-8')   # 发送者
+message['From'] = Header(u' <serivces@jnyhldw.com>', 'UTF-8')   # 发送者
 message['To'] = Header(u'<914081010@qq.com>', 'UTF-8')    # 接收者
 message['Subject'] = Header(subject, 'utf-8')
 
@@ -26,5 +26,6 @@ try:
     service.helo('serivces@jnyhldw.com')
     service.sendmail(sender, receivers, message.as_string())
     print("邮件发送成功")
-except smtplib.SMTPException:
+except smtplib.SMTPException as e:
+    print(e)
     print("Error: 无法发送邮件")
