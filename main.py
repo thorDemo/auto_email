62ec97c6ebcf827786236576ada5b6767f825371
thorDemo <40915413+thorDemo@users.noreply.github.com>
2019-11-19 13:09:02 +0800

    templates

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ content = open('templates/type_1.html', encoding='utf-8')
 message = MIMEText(content.read(), _subtype='html', _charset='utf-8')
 message['Accept-Language'] = "zh-CN"
 message['Accept-Charset'] = "ISO-8859-1,UTF-8"
-message['From'] = Header('宝马会', 'UTF_8')  # 发送者
+message['From'] = Header(('宝马会娱乐城'.encode('utf-8'), 'utf-8'), (b'<serivces@jnyhldw.com>', None))  # 发送者
 message['To'] = Header('<914081010@qq.com>', 'UTF-8')    # 接收者
 message['Subject'] = Header(subject, 'utf-8')
 message['Received'] = 'from msc-channel180022225.sh(100.68.112.227) by smtp.aliyun-inc.com(127.0.0.1);'
