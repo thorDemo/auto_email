eee45215d6e1cdcfe1e1377d2a730d81a5c5dc40
thorDemo <40915413+thorDemo@users.noreply.github.com>
2019-11-19 12:36:52 +0800

    templates

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import uuid
 import smtplib
 from email.mime.text import MIMEText
 from email.header import Header
+from email.headerregistry import Address
 
 sender = 'serivces@jnyhldw.com'
 subject = '宝马会礼金大放送'
@@ -15,13 +16,14 @@ receivers = [
 content = open('templates/type_1.html', encoding='utf-8')
 message = MIMEText(content.read(), _subtype='html', _charset='utf-8')
 message['Accept-Language'] = "zh-CN"
-message['Accept-Charset'] = "ISO-8859-1,utf-8"
-message['From'] = Header(u'宝马会 <serivces@jnyhldw.com>', 'utf-8')    # 发送者
-message['To'] = Header(u'914081010@qq.com', 'utf-8')    # 接收者
+message['Accept-Charset'] = "ISO-8859-1,UTF-8"
+message['From'] = Address("宝马会", "pepe", "jnyhldw.com")   # 发送者
+message['To'] = Header(u'<914081010@qq.com>', 'UTF-8')    # 接收者
 message['Subject'] = Header(subject, 'utf-8')
 
 try:
     service = smtplib.SMTP('localhost')
+    service.helo('serivces@jnyhldw.com')
     service.sendmail(sender, receivers, message.as_string())
     print("邮件发送成功")
 except smtplib.SMTPException:
