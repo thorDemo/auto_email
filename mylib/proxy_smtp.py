4c6b311b159b84d9649d1b481c3ffd30387b0e05
thorDemo <40915413+thorDemo@users.noreply.github.com>
2019-11-20 16:33:40 +0800

    thread pool 5

---
+++ mylib/proxy_smtp.py
@@ -0,0 +1,75 @@
+import socket
+import ssl
+import base64
+import time
+import os
+import random
+
+
+# Exception classes used by this module.
+class SMTPException(OSError):
+    """Base class for all exceptions raised by this module."""
+
+
+class SMTPServerDisconnected(SMTPException):
+    """Not connected to any SMTP server.
+
+    This exception is raised when the server unexpectedly disconnects,
+    or when an attempt is made to use the SMTP instance before
+    connecting it to a server.
+    """
+
+
+class ProxySMTP:
+
+    def __init__(self, proxy_ip):
+        self._true_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self._true_socket.bind((proxy_ip, 0))
+        self.command_encoding = 'ascii'
+        self.__ssl_client_Socket = object
+
+    def socket_connect(self):
+        self.__ssl_client_Socket = ssl.wrap_socket(
+            self._true_socket,
+            cert_reqs=ssl.CERT_NONE,
+            ssl_version=ssl.PROTOCOL_SSLv23
+        )
+        self.__ssl_client_Socket.connect(('smtp.qq.com', 465))
+        res = self.__ssl_client_Socket.recv(1024).decode('utf-8')
+        print(res)
+
+    def send_mail(self, _sender, _receivers):
+        self.socket_connect()
+
+    def hello(self):
+        # self.__ssl_client_Socket.send(b'HELO qq.com\r\n')
+        # res = self._true_socket.recv(1024)
+        # if res:
+        #     data = str(res, encoding='utf-8')
+        #     data = data.split('\r\n')
+        #     for line in data:
+        #         if 'title' in line:
+        #             print(line)
+        # else:
+        #     print(f"visit {0} error")
+        pass
+
+    def send(self, s):
+        """Send `s' to the server."""
+        if hasattr(self, 'sock') and self.sock:
+            if isinstance(s, str):
+                # send is used by the 'data' command, where command_encoding
+                # should not be used, but 'data' needs to convert the string to
+                # binary itself anyway, so that's not a problem.
+                s = s.encode(self.command_encoding)
+            try:
+                self.sock.sendall(s)
+            except OSError:
+                self._true_socket.close()
+                raise SMTPServerDisconnected('Server not connected')
+        else:
+            raise SMTPServerDisconnected('please run connect() first')
+
+
+ps = ProxySMTP('128.14.154.138')
+ps.send_mail('1935940593@qq.com', '')
---
+++ test_socket.py
@@ -0,0 +1,33 @@
+import socket
+import re
+
+host = "2000019.ip138.com"
+port = 80
+ip = socket.gethostbyname(host)
+address = (ip, port)
+try:
+    local_ip = '209.73.145.170'
+    print(f'设置本地IP：{local_ip}')
+    true_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    true_socket.bind((local_ip, 0))
+    # s = socket.socket()
+    s = true_socket
+    s.connect(address)
+    request = "GET / HTTP/1.1\r\n" \
+              "Host: 2000019.ip138.com\r\n" \
+              "User-Agent: Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) " \
+              "Chrome/44.0.2403.125 Safari/537.36\r\n" \
+              "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3\r\n\r\n"
+    s.sendall(request.encode("utf-8"))
+    res = s.recv(4096)
+    # print(res)
+    if res:
+        data = str(res, encoding='utf-8')
+        data = data.split('\r\n')
+        for line in data:
+            if 'title' in line:
+                print(line)
+    else:
+        print("visit {0} error".format(host))
+except Exception as e:
+    raise e
