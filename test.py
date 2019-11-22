02460e32ced0e9095ff896bebccaabf633cba9fd
thorDemo <40915413+thorDemo@users.noreply.github.com>
2019-11-22 15:28:05 +0800

    rsa

--- test.py
+++ test.py
@@ -57,7 +57,7 @@ class SMTPError(SMTP):
 _domain = 'bmw1984.com'
 _receivers = 'thorhx@gmail.com'
 logging = Logger('send_email.log').get_log()
-_sender = f'service_{random_chars(4)}@{_domain}'
+_sender = f'service@{_domain}'
 content = open('templates/type_1.html', encoding='utf-8')
 message = MIMEText(content.read(), _subtype='html', _charset='utf-8')
 content.close()
@@ -66,26 +66,26 @@ message['Accept-Charset'] = "ISO-8859-1,UTF-8"
 message['From'] = encode_header('宝马会娱乐城', _sender)
 message['To'] = encode_header('超级VIP客户', _receivers)
 message['Subject'] = Header('宝马会礼金大放送', 'utf-8')
-message['Received'] = f'from msc-channel180022225.sh(100.68.112.227) by smtp.{_domain}(127.0.0.1);'
+message['Received'] = f'from msc-channel180022225.sh(128.14.154.138) by mail.{_domain}(127.0.0.1);'
 message['Message-ID'] = uuid.uuid4().__str__()
 message['MIME-Version'] = '1.0'
-message['Return-Path'] = f'smtp.{_domain}'
+message['Return-Path'] = f'mail.{_domain}'
 service = SMTPError('localhost')
 # service.ehlo()
-DKIM_SELECTOR = 's1'
-DKIM_DOMAIN = 'bmw1984.com'
+dkim_selector = b's1'
+dkim_domain = b'mail.bmw1984.com'
 with open('conf/rsaky.pem') as fh:
     DKIM_PRIVATE_KEY = fh.read()
     # lines = re.split(b"\r?\n", bytes(DKIM_PRIVATE_KEY, encoding='utf-8'))
     # print(lines)
     signature = dkim.sign(
         message=message.as_bytes(),
-        selector=b's1',
-        domain=b'bmw1984.com',
+        selector=dkim_selector,
+        domain=dkim_domain,
         privkey=bytes(DKIM_PRIVATE_KEY, encoding='utf-8'),
     )
     message['DKIM-Signature'] = bytes.decode(signature.lstrip(b"DKIM-Signature: "))
-    print(message.as_string())
+    # print(message.as_string())
     data = service.sendmail(_sender, _receivers, message.as_string())
     logging.info(f'{_receivers} 邮件发送成功！ {data}')
 
