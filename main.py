7114a2265fcce71ec15dacfc567a20e5c0b18cbc
thorDemo <40915413+thorDemo@users.noreply.github.com>
2019-11-25 13:04:26 +0800

    socket

--- main.py
+++ main.py
@@ -5,11 +5,12 @@ from email.mime.text import MIMEText
 from email.header import Header
 from mylib.coder import encode_header
 from mylib.code_logging import Logger
+import time
 
 
 emails = open('target/111901.txt')
 temp = 0
-for email in emails:
+for line in emails:
     if temp % 20 == 0:
         _receivers = '914081010@qq.com'
         _domain = 'bmw1984.com'
@@ -22,7 +23,7 @@ for email in emails:
         message['Accept-Charset'] = "ISO-8859-1,UTF-8"
         message['From'] = encode_header('宝马会娱乐城', _sender)
         message['To'] = encode_header('超级VIP客户', _receivers)
-        message['Subject'] = Header('宝马会礼金大放送', 'utf-8')
+        message['Subject'] = Header(f'service<1> renter:<{temp}>', 'utf-8')
         message['Received'] = f'from msc-channel180022225.sh(128.14.154.138) by mail.{_domain}(127.0.0.1);'
         message['Message-ID'] = uuid.uuid4().__str__()
         message['MIME-Version'] = '1.0'
@@ -32,16 +33,18 @@ for email in emails:
         dkim_selector = 's1'
         dkim_domain = 'bmw1984.com'
         status, code, msg = service.send_mail(_sender, _receivers, message, dkim_key, dkim_selector, dkim_domain)
-        if status == 250:
+        if code == 250:
             logging.info(f'{_receivers} 邮件发送成功！ {status, code, msg}')
         else:
             logging.warning(f'{_receivers} 邮件发送失败！ {status, code, msg}')
+        temp += 1
+        time.sleep(4)
     else:
         _domain = 'bmw1984.com'
-        _receivers = email
+        _receivers = line.strip()
         logging = Logger('send_email.log').get_log()
         _sender = f'service@{_domain}'
-        content = open('templates/type_1.html', encoding='utf-8')
+        content = open('templates/type_2.html', encoding='utf-8')
         message = MIMEText(content.read(), _subtype='html', _charset='utf-8')
         content.close()
         message['Accept-Language'] = "zh-CN"
@@ -58,7 +61,9 @@ for email in emails:
         dkim_selector = 's1'
         dkim_domain = 'bmw1984.com'
         status, code, msg = service.send_mail(_sender, _receivers, message, dkim_key, dkim_selector, dkim_domain)
-        if status == 250:
+        if code == 250:
             logging.info(f'{_receivers} 邮件发送成功！ {status, code, msg}')
         else:
             logging.warning(f'{_receivers} 邮件发送失败！ {status, code, msg}')
+        temp += 1
+        time.sleep(4)
